32db4c66d0c2da2abbf5aa7455f3785fa7ee0846
jjaebbi <wangyeonk@gmail.com>
2023-12-13 04:05:20 +0900

    test 오류 안나게만 수정..

--- src/test/java/taba/dajoba/service/JobPostingServiceTest.java
+++ src/test/java/taba/dajoba/service/JobPostingServiceTest.java
@@ -3,7 +3,11 @@ package taba.dajoba.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import taba.dajoba.controller.JobPostingForm;
 import taba.dajoba.domain.JobPosting;
 import taba.dajoba.repository.JobPostingRepository;
 
@@ -22,20 +26,12 @@ class JobPostingServiceTest {
     public void 채용공고조회() throws Exception {
         //given
 
+        Pageable pageable = PageRequest.of(4, 20);
 
         //when
-        List<JobPosting> jobpostings = jobPostingService.showAllFrequent();
+        Page<JobPostingForm> jobpostings = jobPostingService.showAllJobPostings(pageable);
 
         //then
-        JobPosting jobposting = jobpostings.get(1);
-        System.out.println(jobposting.getJobPostingUrl());
-        System.out.println(jobposting.getId());
-        System.out.println(jobposting.getJobGroup());
-        System.out.println(jobposting.getCompany());
-        System.out.println(jobposting.getRecruitDeadline());
-        System.out.println(jobposting.getMainduties());
-        System.out.println(jobposting.getTitle());
-        System.out.println(jobposting.getBenefits());
 
     }
 
